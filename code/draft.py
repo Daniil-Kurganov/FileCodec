a841fa2205cb6a042d3cdf7d793c3433b5faeefb
Daniil Kurganov <dk30128@gmail.com>
2024-05-10 19:29:43 +0300

    Реализация кодека

--- code/draft.py
+++ code/draft.py
@@ -1,7 +1,7 @@
+import docx
 from HammingCodec import hamming_codec_encode, hamming_codec_decode
 
-s = 'Я так хотел остаться...'
-z = hamming_codec_encode(s)
-print(z)
-r = hamming_codec_decode(z)
-print(r)
+file_source, file_result = docx.Document('C:/Users/User/PythonProjects/FileCodec/files/WordFileEncode.docx'), docx.Document()
+for paragraph_current in file_source.paragraphs:
+    file_result.add_paragraph(hamming_codec_decode(paragraph_current.text))
+file_result.save('C:/Users/User/PythonProjects/FileCodec/files/WordFileDecode.docx')
