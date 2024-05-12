07515775d39dfd2f27ad1c3373e5bdf837255008
Daniil Kurganov <dk30128@gmail.com>
2024-05-12 18:09:18 +0300

    Реализация обработок ошибок

--- code/Main.py
+++ code/Main.py
@@ -1,7 +1,8 @@
 import sys
+import os
 import docx
 import openpyxl
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from HammingCodec import hamming_codec_encode, hamming_codec_decode
 from GUI import *
 
@@ -15,8 +16,67 @@ def  set_file_source_path() -> None:
 def set_directory_result_path() -> None:
     '''Установка пути каталога назначения'''
     directory = QFileDialog.getExistingDirectory(caption = 'Выберите каталог назначения',
-                                                            directory = 'C:/Users/User/PythonProjects/FileCodec', options = QFileDialog.ShowDirsOnly)
+                                                        directory = 'C:/Users/User/PythonProjects/FileCodec', options = QFileDialog.ShowDirsOnly)
     ui.TextEditFileResultPath.setText(directory)
+    return None
+def show_messages(float_message_key: float, string_message_text = None) -> None:
+    '''Сообщений в зависимости от ключа. Формат словаря: {код: [иконка, название окна, заголовок, текст], ...}'''
+    dictionary_messages_settings = {
+        0.0: [QMessageBox.Information, 'Успех!', 'Кодирование завершилось успешно', 'Файл обработки успешно закодирован и сохранён.'],
+        0.1: [QMessageBox.Information, 'Успех!', 'Декодирование завершилось успешно', 'Файл обработки успешно декодирован и сохранён.'],
+        6.0: [QMessageBox.Warning, 'Внимание!', 'Файл назначения уже существует', 'Файл назначения будет перезаписан.'],
+        13.0: [QMessageBox.Critical, 'Ошибка!', 'Ошибка ввода путей файлов', 'Пути файлов совпадают.'],
+        13.11: [QMessageBox.Critical, 'Ошибка!', 'Ошибка ввода пути исходного файла', 'Файл обработки не существует.'],
+        13.12: [QMessageBox.Critical, 'Ошибка!', 'Ошибка ввода пути исходного файла', 'Расширение файла обработки не поддерживается.'],
+        13.13: [QMessageBox.Critical, 'Ошибка!', 'Ошибка ввода пути исходного файла', string_message_text],
+        13.21: [QMessageBox.Critical, 'Ошибка!', 'Ошибка ввода пути файла назначения', 'Директория назначения не существует.'],
+        13.22: [QMessageBox.Critical, 'Ошибка!', 'Ошибка ввода пути файла назначения', 'Расширение файла назначения не поддерживается.'],
+        13.23: [QMessageBox.Critical, 'Ошибка!', 'Ошибка ввода пути файла назначения', string_message_text],
+    }
+    message = QMessageBox()
+    message.setIcon(dictionary_messages_settings[float_message_key][0])
+    message.setWindowTitle(dictionary_messages_settings[float_message_key][1])
+    message.setText(dictionary_messages_settings[float_message_key][2])
+    message.setInformativeText(dictionary_messages_settings[float_message_key][3])
+    message.exec_()
+    return None
+def checking_file_paths(string_filepath_source: str, string_filepath_result: str) -> bool:
+    '''Провекра корекстности введённых путей'''
+    if string_filepath_source == string_filepath_result:
+        show_messages(13.0)
+        return False
+    try:
+        if not os.path.isfile(string_filepath_source):
+            show_messages(13.11)
+            return False
+        string_path, string_extension = os.path.splitext(string_filepath_source)
+        if string_extension[1:] not in ['txt', 'docx', 'xlsx']:
+            show_messages(13.12)
+            return False
+    except Exception as error:
+        show_messages(13.13, string_message_text = error)
+        return False
+    try:
+        if not os.path.isdir(string_filepath_result[:string_filepath_result.rfind('/')]):
+            show_messages(13.21)
+            return False
+        string_path, string_extension = os.path.splitext(string_filepath_result)
+        if string_extension[1:] not in ['txt', 'docx', 'xlsx']:
+            show_messages(13.22)
+            return False
+        if os.path.isfile(string_filepath_result):
+            show_messages(6.0)
+            return False
+    except Exception as error:
+        show_messages(13.23, string_message_text = error)
+        return False
+
+    return True
+def start_working_codec() -> None:
+    '''Старт работы кодека'''
+    global bool_operation_is_encode
+    string_filepath_source, string_filepath_result = ui.TextEditFileSourcePath.toPlainText(), ui.TextEditFileResultPath.toPlainText()
+    if not checking_file_paths(string_filepath_source, string_filepath_result): return None
     return None
 def set_encode_type_operation() -> None:
     '''Установка типа операции: кодирование'''
@@ -43,6 +103,7 @@ if __name__ == '__main__':
     ui.ToolButtonSetDirectoryResultPath.clicked.connect(set_directory_result_path)
     ui.RadioButtonEncodeType.clicked.connect(set_encode_type_operation)
     ui.RadioButtonDecodeType.clicked.connect(set_decode_type_operation)
+    ui.PushButtonStart.clicked.connect(start_working_codec)
     sys.exit(app.exec_())
 
 
